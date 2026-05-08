63a00d55f661305760cf878a4344c2214e6196b6
Alessio Lombardo <mr.alessiolombardo@gmail.com>
2026-05-08 11:54:52 +0200

    Revert "Aggiunta progetto Ionic pg_frontend"
    This reverts commit dca2bd244e59fbe823c4cf6eee5c00c2644de929.

--- pg_frontend/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'pg_frontend',
-  webDir: 'www'
-};
-
-export default config;
